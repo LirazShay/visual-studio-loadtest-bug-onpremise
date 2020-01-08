ded212f6b036822c98a940baca7a2c3e0535c503
Liraz Shay <liraz@om2.com>
2020-01-08 16:16:31 +0200

    Initial commit

---
+++ DemoLoadTestProject/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoLoadTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var x = "fds";
+            Console.WriteLine(x);
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var x = "fds";
+            Console.WriteLine(x);
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var x = "fds";
+            Console.WriteLine(x);
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
